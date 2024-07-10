c36af39a37044e472a16f0873a21c3ebe4b0f8d2
Linus Horn <linushorn@gmx.net>
2024-07-10 16:34:11 +0200

    added ETA and progress bar

--- dewiki_functions.py
+++ dewiki_functions.py
@@ -1,15 +1,15 @@
 from threading import Thread
 import json
-import re
+import time
+import datetime
 from html2text import html2text as htt
 import wikitextparser as wtp
-from tqdm import tqdm
 
 
 def dewiki(text):
     text = wtp.parse(text).plain_text()  # wiki to plaintext 
     text = htt(text)  # remove any HTML
-    text = text.replace('\\n',' ')  # replace newlines
+    text = text.replace('\n',' ')  # replace newlines
     text = text.strip()  # remove leading/trailing whitespace
     return text
 
@@ -42,13 +42,36 @@ def save_article(article, savedir):
             json.dump(doc, outfile, sort_keys=True, indent=1, ensure_ascii=False)
 
 
+
+estimated_n_articles = 6_843_803
 def process_file_text(filename, savedir):
-    article = ''
+    article_lines = []
     with open(filename, 'r', encoding='utf-8') as infile:
-        for line in tqdm(infile):
+        
+        timestamps = []
+
+        for line in infile:
             if '<page>' in line:
-                article = ''
+                article_lines = [line]
             elif '</page>' in line:  # end of article
+                article = " ".join(article_lines)
                 Thread(target=save_article, args=(article, savedir)).start()
+                timestamps.append(time.time())
+
+                n_articles_processed = len(timestamps)
+
+                n_rolling_avg = 100
+                if n_articles_processed > n_rolling_avg and n_articles_processed % 100 == 0:
+                    last_10 = timestamps[-n_rolling_avg:]
+                    avg_time = (last_10[-1] - last_10[0]) / n_rolling_avg
+                    time_remaining = (estimated_n_articles - n_articles_processed) * avg_time
+                    time_remaining = max(time_remaining, 0) # negitve time will throw an error
+                    time_remaining_str = datetime.datetime.fromtimestamp(time_remaining).strftime('%H:%M:%S')
+
+                    percentage = n_articles_processed / estimated_n_articles * 100
+                    
+                    _str = f'{n_articles_processed} / {estimated_n_articles} articles processed ({percentage:.2f}%) ETA: {time_remaining_str}'
+                    print(_str, end='\r')
+
             else:
-                article += line
+                article_lines.append(line)
